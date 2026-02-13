be8e4b10e81c54ef50bbc3072017cde922fe4b19
Giacomo Nanni <gn.nanni@gmail.com>
2026-02-13 17:17:57 +0100

    test: silbersteinstrasse

--- src/lib/story-catalog.js
+++ src/lib/story-catalog.js
@@ -4,11 +4,16 @@ const storyCatalog = [
     title: 'Genova 2001',
     storyPath: '/data/g8/story.json'
   },
-  {
-    slug: 'hermann',
-    title: 'Hermannstrasse',
-    storyPath: '/data/hermann/story.json'
+   {
+    slug: 'silber',
+    title: 'SilbersteinStraße',
+    storyPath: '/data/silber/story.json'
   }
+  // {
+  //   slug: 'hermann',
+  //   title: 'Hermannstrasse',
+  //   storyPath: '/data/hermann/story.json'
+  // }
 ]
 
 const getStoryBySlug = (slug) => storyCatalog.find((story) => story.slug === slug) ?? null
