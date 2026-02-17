39be09f451010c6dd0b4e12ee730a1264584e9ee
Giacomo Nanni <gn.nanni@gmail.com>
2026-02-17 10:44:16 +0100

    chore fallback stories

---
+++ src/routes/stories/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { base } from '$app/paths'
+  import { storyCatalog } from '$lib/story-catalog'
+</script>
+
+<svelte:head>
+  <title>Vantage Stories</title>
+</svelte:head>
+
+<main class="mx-auto max-w-xl text-black py-4">
+  {#each storyCatalog as story (story.slug)}
+    <a href={`${base}/stories/${story.slug}`}>
+      <h1 class="text-xl font-normal pb-2">{story.title}</h1>
+    </a>
+  {/each}
+</main>
