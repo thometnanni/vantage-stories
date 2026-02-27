2a340ecb3d1c83bfebd9d7e57ed2adc2842e5a9b
Giacomo Nanni <gn.nanni@gmail.com>
2026-02-27 09:39:33 +0100

    fix: ignore stories, redirict back /

--- src/routes/stories/+page.js
+++ src/routes/stories/+page.js
@@ -1,1 +1,8 @@
+import { redirect } from '@sveltejs/kit'
+import { base } from '$app/paths'
+
 export const prerender = true
+
+export const load = () => {
+  throw redirect(308, `${base}/`)
+}
--- src/routes/stories/+page.svelte
+++ src/routes/stories/+page.svelte
@@ -1,12 +1,3 @@
-<script>
-  import Cards from '$lib/components/Cards.svelte'
-  import { storyCatalog } from '$lib/story-catalog'
-</script>
-
 <svelte:head>
   <title>Vantage Stories</title>
 </svelte:head>
-
-<main class="mx-auto max-w-6xl text-black py-4 px-4">
-  <Cards stories={storyCatalog} heading="Stories" />
-</main>
